12f45ab66e3ef34c8a9dbee11dc710856deb4dff
ARNULFO VELASQUEZ SORIANO <yumkaax@gmail.com>
2021-01-13 15:17:42 -0600

    Before Fix CORS Error

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,8 +1,10 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -20,14 +22,16 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public ActionResult<IEnumerable<AppUser>> GetUsers()
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
-          return _context.Users.ToList();   
+          return await _context.Users.ToListAsync();   
         }
 
         [HttpGet("{id}")]
 
-        public ActionResult<AppUser> GetUser(int id) => _context.Users.Find(id);
+        public async Task<ActionResult<AppUser>> GetUserAsync(int id) {
+          return await _context.Users.FindAsync(id);
+        } 
 
     }
 }
--- API/Startup.cs
+++ API/Startup.cs
@@ -37,10 +37,8 @@ namespace API
              });
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-            });
+            services.AddCors();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,13 +48,18 @@ namespace API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
             }
 
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(policy => 
+                policy.AllowAnyHeader()
+                .AllowAnyMethod()
+                .WithOrigins("http://localhost:4200")
+                .AllowCredentials()
+                .WithOrigins("http://localhost:4200"));
 
             app.UseAuthorization();
 
