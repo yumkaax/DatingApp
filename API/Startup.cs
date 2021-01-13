bc7c8f895b67b88395251b8d139caf87d0c06fde
ARNULFO VELASQUEZ SORIANO <yumkaax@gmail.com>
2021-01-13 10:47:37 -0600

    End of Section 1 Git Test

--- API/Startup.cs
+++ API/Startup.cs
@@ -46,6 +46,7 @@ namespace API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // This is a git test
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
